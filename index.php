52772fce565c5c97df07a2722e1c1a2912426e19
Kevin Regelink <109856528+daysix-nl@users.noreply.github.com>
2023-05-23 12:36:37 +0200

    WIP

--- index.php
+++ index.php
@@ -9,11 +9,8 @@
 get_header(); ?>
 
 <main>
-<section class="bg-diepgrijs pb-4">
-        <div class="container text-wit home-header-section flex flex-col justify-center">
-            <h1 class="text-65 leading-65 md:text-100 md:leading-100 font-medium row-center">Oops you're too fast!</h1>
-            <p class="text-22 leading-30 md:text-28 md:leading-38 font-light pr-4 mt-3">This page does not exist or is being created.</p>
-        </div>
-</section>
+<?php
+header(‘Location: https://tapasdebuurvrouw.nl/’); 
+?>
 </main>
 <?php get_footer(); ?>
